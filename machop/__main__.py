d93791a4b1a5fba8471f52869cd5dfe4e9332cf7
neuroticnerd <eggleton.bryce@gmail.com>
2014-07-07 15:21:46 -0400

    resolved issues with loading incorrect karatechop

--- machop/__main__.py
+++ machop/__main__.py
@@ -1,32 +1,37 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import imp
 import machop
+
 
 def main():
     """
     creates the CLI for machop
     """
-    # config = os.path.join(os.getcwd(), 'karatechop.py')
+    CONFIG_MODULE = 'karatechop'
+    CONFIG_FILENAME = CONFIG_MODULE + '.py'
+    invocation_path = sys.argv[0]
     args = sys.argv[1:]
     exit_code = 0
+    CWD = os.getcwd()
+    karatechop = None
     try:
-        setup = __import__('karatechop')
-    except ImportError as e:
+        meta = ('.py', 'rb', imp.PY_SOURCE)
+        filepath = os.path.join(CWD, CONFIG_FILENAME)
+        with open(filepath, 'rb') as kfile:
+            karatechop = imp.load_module(CONFIG_MODULE, kfile, filepath, meta)
+    except (ImportError, IOError):
         print "\nconfig module could not be loaded!\n"
         print "hint: is your config in the current working directory?"
         print "hint: is your config named 'karatechop.py'?"
         exit_code = 1
         raise SystemExit(exit_code > 0)
-    cwd = os.getcwd()
     if len(args) > 0:
         for command in args:
             # @@@ TODO use argparse to config params to commands
-            machop.run(command, cmdpath=cwd)
+            machop.run(command, cmdpath=CWD)
     else:
         # @@@ TODO use argparse to config params to commands
-        machop.run('focus-energy', cmdpath=cwd)
+        machop.run('focus-energy', cmdpath=CWD)
     raise SystemExit(exit_code > 0)
-
-if __name__ == "__main__":
-    main()
